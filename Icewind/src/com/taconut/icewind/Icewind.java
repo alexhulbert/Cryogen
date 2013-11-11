7916709d6a4ddb933ac454badb7bb67a273803c7
Alex Hulbert <alex@alexhulbert.com>
2013-11-10 21:03:39 -0500

    Added Auth Plist parsers and settings retriever Started backupudid thing, but I am really stuck here

--- Icewind/src/com/taconut/icewind/Icewind.java
+++ Icewind/src/com/taconut/icewind/Icewind.java
@@ -1,12 +1,14 @@
 package com.taconut.icewind;
-import com.taconut.icewind.iCloud;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
 /**
  *
  * @author alex_000
  */
 public class Icewind {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
     }
-    
 }
