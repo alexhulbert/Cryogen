43d8258f73b3dd8bc20c8c8a62a9c02b33d425e4
PythEch <pythech.tr@gmail.com>
2014-07-09 13:46:56 +0300

    ChunkServer Protobuf
    This seems to be needed, but I'm not sure at the moment... Also I
    uploaded a fixed version of jython, has heapq.py encoded as UTF-8
    instead of Latin-1.

--- code/protobuild.sh
+++ code/protobuild.sh
@@ -1,3 +1,7 @@
 rm ../Icewind/src/com/alexhulbert/icewind/Protocol.java
 ./bin/protoc --java_out=../Icewind/src/com/alexhulbert/icewind  protocol.proto
 sed -i "1ipackage com.alexhulbert.icewind;\n" ../Icewind/src/com/alexhulbert/icewind/Protocol.java
+
+rm ../Icewind/src/com/alexhulbert/icewind/ChunkServer.java
+./bin/protoc --java_out=../Icewind/src/com/alexhulbert/icewind  chunkserver.proto
+sed -i "1ipackage com.alexhulbert.icewind;\n" ../Icewind/src/com/alexhulbert/icewind/ChunkServer.java
