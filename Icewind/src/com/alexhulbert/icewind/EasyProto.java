8638302b86fa8296375fe86a56f3fa896dc05fee
Triforce1 <alex@alexhulbert.com>
2014-07-10 15:24:41 -0400

    Added a little to EasyProto DOCUMENTATION!!!!

--- Icewind/src/com/alexhulbert/icewind/EasyProto.java
+++ Icewind/src/com/alexhulbert/icewind/EasyProto.java
@@ -33,19 +33,35 @@ import java.util.logging.Logger;
 public class EasyProto<T extends GeneratedMessage> {
     private final byte[] inp;
     private final Parser p;
+    
+    /**
+     * A class for easily parsing/decoding Varint-encoded messages.
+     * Should include the downloading part as well if I get around to it.
+     * @param parser USe Protocol.T.PARSER to get this. Used to parse the messages
+     * @param input The stream of bytes that will be decrypted.
+     */
     public EasyProto(Parser<T> parser, byte[] input) {
         inp = input;
         p = parser;
     }
     
+    /**
+     * Decode a single, protobuf-encoded Message
+     * @return a decoded instance of type T
+     * @throws InvalidProtocolBufferException Doesn't fit the protobuf
+     */
     public T parse() throws InvalidProtocolBufferException {
         return (T) p.parseFrom(inp);
     }
     
+    /**
+     * Parse and decode a varint-delimited, protobuf-encoded Message array
+     * @return An array of T messages
+     */
     public T[] parseVarint() {
         ByteArrayInputStream bais = new ByteArrayInputStream(inp);
         List<T> lst = new ArrayList<T>();
-        while(bais.available() > 0) {
+        while (bais.available() > 0) {
             try {
                 lst.add((T) p.parseDelimitedFrom(bais));
             } catch (InvalidProtocolBufferException ex) {}
@@ -53,6 +69,10 @@ public class EasyProto<T extends GeneratedMessage> {
         return (T[]) lst.toArray();
     }
     
+    /**
+     * How many varint-encoded messages are contained within the input
+     * @return The number of messages
+     */
     public long size() {
         long s = 0;
         ByteArrayInputStream bais = new ByteArrayInputStream(inp);
@@ -63,5 +83,33 @@ public class EasyProto<T extends GeneratedMessage> {
             } catch (InvalidProtocolBufferException ex) {}
         }
         return s;
+    }
+    
+    /**
+     * This is a little slower, but you can use it with those pretty progress bars. No more boring throbbers!
+     * @param lb The LoadingBar instance to write to
+     * @param loadText The prefixed
+     * @return An array of T messages
+     */
+    public T[] parseVarint(LoadingBar lb, String loadText) {
+        ByteArrayInputStream counter = new ByteArrayInputStream(inp);
+        ByteArrayInputStream parser  = new ByteArrayInputStream(inp); //Pass-by-reference
+        long totalCount = 0;
+        List<T> output = new ArrayList<T>();
+        while (counter.available() > 0) {
+            try {
+                p.parseDelimitedFrom(counter);
+                totalCount++;
+            } catch (InvalidProtocolBufferException ex) {}
+        }
+        
+        for (int i = 0; i < totalCount; i++) {
+            try {
+                output.add((T) p.parseDelimitedFrom(parser));
+                lb.progress((i*100)/totalCount);
+                lb.status(loadText + String.format(" (%s/%s)", i, totalCount));
+            } catch (InvalidProtocolBufferException ex) {}
+        }
+        return (T[]) output.toArray();
     }
 }
