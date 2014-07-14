524a4ce1b7c7501361c22cb094a977a0d19b5f3e
Triforce1 <alex@alexhulbert.com>
2014-07-14 15:00:41 -0400

    Tying loose ends Added ghost library, removed REBASE, and got rid of chunkserver stuff

--- code/protobuild.sh
+++ code/protobuild.sh
@@ -1,7 +1,3 @@
 rm ../Icewind/src/com/alexhulbert/icewind/Protocol.java
 ./bin/protoc --java_out=../Icewind/src/com/alexhulbert/icewind  protocol.proto
 sed -i "1ipackage com.alexhulbert.icewind;\n" ../Icewind/src/com/alexhulbert/icewind/Protocol.java
-
-rm ../Icewind/src/com/alexhulbert/icewind/ChunkServer.java
-./bin/protoc --java_out=../Icewind/src/com/alexhulbert/icewind  chunkserver.proto
-sed -i "1ipackage com.alexhulbert.icewind;\n" ../Icewind/src/com/alexhulbert/icewind/ChunkServer.java
