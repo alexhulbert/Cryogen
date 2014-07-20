5646320e5a466db0f6f222bd77997fb2bc03ae05
danieltroger <daniel@natur-kultur.eu>
2014-07-20 10:36:47 +0200

    fix?
    did the previous script even work? Didn't it download the source which doesn't contain the finished jar?

--- code/Rev-Eng/keychain-sshrd.sh
+++ code/Rev-Eng/keychain-sshrd.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-wget --no-check-certificate https://github.com/msftguy/ssh-rd/archive/rev-04b.zip
-unzip rev-04b.zip
-java -jar ssh-rd.jar #Different name?
-rm rev-04b.zip ssh-rd.jar #Is there a readme in that zip too?
+wget --no-check-certificate https://github.com/downloads/msftguy/ssh-rd/ssh_rd_rev04a.jar -O ssh_rd.jar
+java -jar ssh-rd.jar 
