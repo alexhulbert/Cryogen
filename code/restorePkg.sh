8a99e1811b60acc6418ded30ed29bcbf316153cb
danieltroger <daniel@natur-kultur.eu>
2014-07-21 10:43:07 +0200

    Not sure if this works....
    Don't even really know the structure of backup.zip lol and why supply all debs manually? wouldn't it be enough with apt-get and depencies and then --fix-missing? And am i right with that unzip needs to target to /tmp/icewind ? Someone should try this lol

--- code/restorePkg.sh
+++ code/restorePkg.sh
@@ -1,11 +1,16 @@
-unzip backup.zip /tmp/icewind
-dpkg --set-selections << /tmp/icewind/sel
-dpkg -i /tmp/icewind/apt07strict/* #constains apt-get packages & dependencies
-cp /tmp/icewind/sources /path/to/sources/file
+unzip -q backup.zip -d /tmp/icewind
+dpkg --log /var/log/icew1nd.log -i /tmp/icewind/apt07strict/* #constains apt-get packages & dependencies
+cp -R /tmp/icewind/sources/* /etc/apt/sources.list.d/
 apt-get update
-dpkg -i /tmp/icewind/debs/*
-rm -R /tmp/icewind/debs/*
-rmdir /tmp/icewind/debs
-rm /tmp/icewind/sel
+dpkg --set-selections << /tmp/icewind/sel
 apt-get --fix-missing -f dselect-upgrade
 reboot
+#structure yet as far as I understood
+#   backup.zip #must be in the same dir as restorePkg.sh atm
+#      ./apt07strict/
+#        apt-get and depencies
+#      ./sources/
+#        cydia.list
+#        saurik.list # shall we even restre this one? Shit the same everywhere
+#        whatever.list
+#      ./sel
